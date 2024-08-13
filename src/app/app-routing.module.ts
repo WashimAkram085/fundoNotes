d2caca14bafb01687ffe47588c5f63b1f0f955b5
Washim Akram <Washim.Akram@godigit.com>
2024-08-13 15:26:53 +0530

    while opening the website it will automatically redirect you to the log in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
+  {path : '', redirectTo:'/login', pathMatch:'full'},
   { path: 'login', component: LoginComponent },
   {path : 'register', component: RegisterComponent}
 ];
