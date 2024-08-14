01cc118c6df97404451df9f9f2a45b6536ae50d4
Washim Akram <Washim.Akram@godigit.com>
2024-08-14 11:27:22 +0530

    footer updated with dropdown features in location in both login and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { NameComponent } from './components/basicInformation/name/name.component';
 import { BirthdayComponent } from './components/basicInformation/birthday/birthday.component';
 import { PasswordComponent } from './components/basicInformation/password/password.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -36,7 +38,10 @@ import { PasswordComponent } from './components/basicInformation/password/passwo
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
