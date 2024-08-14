777e9f1492bf635f2bad25746dd5d4d8237a3224
Washim Akram <Washim.Akram@godigit.com>
2024-08-14 14:51:58 +0530

    create button with 3 functionality in login page added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  openNewAccount(e: Event) {
+    e.preventDefault();
+    //@ts-ignore
+    document.getElementById('newcard').style.display = 'block';
+  }
 }
