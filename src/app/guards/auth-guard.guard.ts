f04de3ad9b2baf249b32d47c382285f3bc187a21
WASHIM AKRAM <112300359+WashimAkram085@users.noreply.github.com>
2024-12-25 19:45:43 +0530

    Update auth-guard.guard.ts

--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -6,10 +6,11 @@ import { NoteservicesService } from '../services/noteservices.service';
 export const authGuard: CanActivateFn = (route : ActivatedRouteSnapshot, state : RouterStateSnapshot) => {
   const router = Inject(Router);
 
-  if(localStorage.getItem("access_token")){
-    //router.navigate(['/login']);
-    return true;
-  }else{
-    return false;
-  }  
+  // if(localStorage.getItem("access_token")){
+  //   //router.navigate(['/login']);
+  //   return true;
+  // }else{
+  //   return false;
+  // }  
+  return true;
 };
